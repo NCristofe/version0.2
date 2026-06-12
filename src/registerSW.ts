b5ec2acb5f65a0d30de12f46b8632f92daab07d3
natanael731 <cristofegit@gmail.com>
2026-06-12 08:55:48 -0300

    feat: create registerSW.ts

---
+++ src/registerSW.ts
@@ -0,0 +1,11 @@
+export function register() {
+  if ('serviceWorker' in navigator && process.env.NODE_ENV === 'production') {
+    window.addEventListener('load', () => {
+      navigator.serviceWorker.register('/sw.js').then(registration => {
+        console.log('SW registrado com sucesso:', registration.scope);
+      }).catch(error => {
+        console.log('Falha ao registrar SW:', error);
+      });
+    });
+  }
+}

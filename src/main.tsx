916a221a00b1d17794b30bc7cc091bc777176ff0
natanael731 <cristofegit@gmail.com>
2026-06-12 08:56:54 -0300

    update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,10 @@
   import { createRoot } from "react-dom/client";
   import App from "./app/App.tsx";
   import "./styles/index.css";
+  import { register } from "./registerSW";
+
+  // Registrar Service Worker para PWA
+  register();
 
   createRoot(document.getElementById("root")!).render(<App />);
   

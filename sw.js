1ff83be11cef49e545e93d97f94d7d1ad2187260
natanael731 <cristofegit@gmail.com>
2026-06-12 08:55:40 -0300

    feat: create sw.js

---
+++ sw.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = 'nosso-amor-cache-v1';
+const ASSETS_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/manifest.json'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request).catch(() => {
+        // Se falhar a rede e não tiver no cache, retorna a página inicial
+        return caches.match('/');
+      });
+    })
+  );
+});

0e75ffa1117abb68787533062a78c5f08f9a380d
NCristofe <cristofegit@gmail.com>
2026-08-31 10:08:46 -0300

    update ProtectedRoute.tsx

--- src/app/components/ProtectedRoute.tsx
+++ src/app/components/ProtectedRoute.tsx
@@ -2,7 +2,11 @@ import { Navigate } from 'react-router';
 import { useAuth } from '../context/AuthContext';
 
 export default function ProtectedRoute({ children }: { children: React.ReactNode }) {
-  const { isAuthenticated, currentUser } = useAuth();
+  const { isAuthenticated, currentUser, loading } = useAuth();
+
+  if (loading) {
+    return null;
+  }
 
   if (!isAuthenticated) {
     return <Navigate to={currentUser ? '/auth' : '/login'} replace />;

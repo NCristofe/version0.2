3954386e2f4ae6b7de7f9842e19939f185663f55
NCristofe <cristofegit@gmail.com>
2026-09-03 10:48:50 -0300

    update App.tsx

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,5 +1,6 @@
 import { RouterProvider } from 'react-router';
 import { AuthProvider } from './context/AuthContext';
+import { PresenceProvider } from './context/PresenceContext';
 import { GamificationProvider } from './context/GamificationContext';
 import { AppDataProvider } from './context/AppDataContext';
 import { Toaster } from './components/ui/sonner';
@@ -9,12 +10,14 @@ export default function App() {
   return (
     <AuthProvider>
       {/* MARKER-MAKE-KIT-INVOKED */}
-      <GamificationProvider>
-        <AppDataProvider>
-          <Toaster position="top-center" richColors />
-          <RouterProvider router={router} />
-        </AppDataProvider>
-      </GamificationProvider>
+      <PresenceProvider>
+        <GamificationProvider>
+          <AppDataProvider>
+            <Toaster position="top-center" richColors />
+            <RouterProvider router={router} />
+          </AppDataProvider>
+        </GamificationProvider>
+      </PresenceProvider>
     </AuthProvider>
   );
 }
